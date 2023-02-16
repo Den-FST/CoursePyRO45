b31c7839359d6c9ac3c5c5444876633489090a02
Denis Sk <dennycim@gmail.com>
2023-02-16 19:06:24 +0000

    16.02.23

--- Interpreters/modul_b.py
+++ Interpreters/modul_b.py
@@ -1,5 +1,46 @@
-import modul_a
-print(modul_a.test)
-courses = ['PE', 'History', 'Math']
-index = modul_a.find_index(courses, 'PE')
-print(index)
+# import modul_a
+
+# from modul_a import find_index, test
+#
+# print(test)
+# # print(modul_a.test)
+# courses = ['PE', 'History', 'Math']
+# index = find_index(courses, 'PE')
+# print(index)
+
+# a patra metoda de import
+
+# from modul_a import find_index, test
+#
+# print(test)
+# # print(modul_a.test)
+# courses = ['PE', 'History', 'Math']
+# index = find_index(courses, 'PE')
+# print(index)
+
+# import pandas, flask
+
+# RECOMANDAT SA SE IMPORTE CITE UN MODUL PER LINIE
+
+# e bine   --- > from modul_a import *
+
+# from modul_a import *
+#
+# var = test
+#
+# print(var)
+
+"""
+Modulele sunt de 3 tipuri :
+- librariile standart os, random, math, sys
+- librariile externe  pandas, tkinter, flask
+- module / librarii in interioriul proiectului   modul_a, modul_b
+"""
+
+# from modul_a import find_index, test
+#
+# print(test)
+# # print(modul_a.test)
+# courses = ['PE', 'History', 'Math']
+# index = find_index(courses, 'PE')
+# print(index)
